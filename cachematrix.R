2adb8cf08d0c476f34d2b5dfe7089b3a9c383781
missmoo <gina_b@hotmail.co.uk>
2015-03-16 17:16:11 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Creates the cache version
-
+## Write a short comment describing this function
+Cache <- 0
+M <- NULL
 makeCacheMatrix <- function(x = matrix()) {
-
+  M <- matrix(solve(x))
+  print("Cache now reads" M)
 }
 
 
-## recalls cache and recalculates
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  Cache <<- M
+  if(Cache==0){
+    print("cache empty.. Calculating..")
+    Cache <- solve(x)
+    Cache
+  Else 
+    Cache
+  }
 }
